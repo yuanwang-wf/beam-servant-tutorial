ced371a0c3ac92f39cb463a21bbb025da5bb60ce
Yuan Wang <yuan@Yuans-MBP.Home>
2019-11-05 21:30:51 -0600

    init repo

---
+++ beam-servant-tutorial.nix
@@ -0,0 +1,13 @@
+{ mkDerivation, aeson, base, stdenv }:
+mkDerivation {
+  pname = "beam-servant-tutorial";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [ aeson base ];
+  executableHaskellDepends = [ base ];
+  doHaddock = false;
+  license = "unknown";
+  hydraPlatforms = stdenv.lib.platforms.none;
+}
