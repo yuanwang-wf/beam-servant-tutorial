736a53618ba752e1902517ae0ebb9f826dd01802
Yuan Wang <yuan.wang@workiva.com>
2019-11-07 08:59:35 -0600

    add zlib

--- beam-servant-tutorial.nix
+++ beam-servant-tutorial.nix
@@ -1,4 +1,6 @@
-{ mkDerivation, aeson, base, stdenv }:
+{ mkDerivation, aeson, base, containers, servant, servant-server
+, stdenv, warp, zlib
+}:
 mkDerivation {
   pname = "beam-servant-tutorial";
   version = "0.1.0.0";
@@ -6,7 +8,10 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [ aeson base ];
-  executableHaskellDepends = [ aeson base ];
+  executableHaskellDepends = [
+    aeson base containers servant servant-server warp
+  ];
+  executableSystemDepends = [zlib];
   doHaddock = false;
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
