a5be63e4600b8939d0a54ab0d19612ca65a53265
Yuan Wang <yuan.wang@workiva.com>
2019-11-26 15:33:50 -0600

    remove toggle-hoogle for now

--- shell.nix
+++ shell.nix
@@ -2,10 +2,16 @@
 let
   inherit (nixpkgs) haskellPackages;
   myPackages = import ./release.nix {inherit nixpkgs compiler; };
+  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
 in
   haskellPackages.shellFor {
     withHoogle = true;
     packages = p: [myPackages];
     buildInputs =  with nixpkgs.haskellPackages;
-     [ hlint stylish-haskell ghcid hoogle];
+     [ hlint 
+       stylish-haskell 
+       ghcid 
+       hoogle
+       (all-hies.selection {selector = p: {inherit (p) ghc865; };}) 
+     ];
 }
